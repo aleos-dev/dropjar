6f22e406fada019349a7c2734f0ec6ffc8e2441d
Aleksei O. <oal.aleos@gmail.com>
2024-11-16 17:25:08 +0200

    feat: implement UserDetails in User model
    Enhanced the User model to implement UserDetails for integration with Spring Security. Added validation annotations, constructors, email equality check, and necessary methods for UserDetails interface implementation.

--- src/main/java/com/aleos/model/User.java
+++ src/main/java/com/aleos/model/User.java
@@ -2,30 +2,88 @@ package com.aleos.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.NaturalId;
+import org.hibernate.proxy.HibernateProxy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
-@Table(name = "users")
-public class User {
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotNull
+    @Size(min = 3, max = 100, message = "Firstname length should be between {min} and {max}")
+    @Column(nullable = false)
+    private String firstname;
+
+    @NotNull
+    @Size(min = 3, max = 100, message = "Lastname length should be between {min} and {max}")
+    @Column(nullable = false)
+    private String lastname;
+
+    @NotNull
     @NaturalId
-    @Email
+    @Email(message = "Invalid email format {value}")
+    @Column(nullable = false, unique = true)
+    @EqualsAndHashCode.Include
     private String email;
 
-    @NonNull
-    @Size(min = 3, max = 100, message = "Password length should be between {min} and {max}")
     private String password;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Role role;
 
+    @NotNull
+    @Column(nullable = false)
+    private Boolean active = false;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = (o instanceof HibernateProxy it) ?
+                it.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = (this instanceof HibernateProxy it) ?
+                it.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        User user = (User) o;
+        return getEmail() != null && Objects.equals(getEmail(), user.getEmail());
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(email);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(role);
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
 }
